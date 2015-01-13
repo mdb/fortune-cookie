4a7d080f3137de78116f6987e0ea2b8b19e0baf6
Mike Ball <mikedball@gmail.com>
2015-01-13 14:28:33 -0500

    add various fortunes

--- fortunes.js
+++ fortunes.js
@@ -1,7 +1,7 @@
 module.exports = function () {
   return [
     "If you have something good in your life, commit!",
-    "A dream you have will come true (in the cloud)",
+    "Your dreams will come true (in the cloud)",
     "You are destined to make the world a better place (through distributed systems)",
     "Try the CMS collector",
     "There is no greater pleasure than red, green, refactor",
@@ -11,6 +11,26 @@ module.exports = function () {
     "The cloud is a place of wonder and mystery",
     "Our builds determine us, as much as we determine our builds",
     "Meeting contention well is the source of your strength",
-    "A rapid HTTP response will soon enter your life with blessings to share"
+    "A rapid HTTP response will soon enter your life with blessings to share",
+    "Learn Haskell",
+    "rm -rf *",
+    "Try restarting the machine",
+    "Depart not from the branch logic which fate has you assigned",
+    "It takes courage to merge",
+    "Long life is in (persistent) store for you",
+    "You will enjoy a RESTful weekend",
+    "REST has a peaceful effect on your physical and emotional health",
+    "RESTing well is as important as working hard",
+    "Someone you care about seeks to merge to master",
+    "Time is precious, but truthiness is more precious than time",
+    "To know oneself, one should assert oneself",
+    "You can keep a secret key",
+    "You can keep a secret token",
+    "You desire conflict resolution and you will find it",
+    "You have exceeded what was expected (in your unit tests)",
+    "Your methods return truth",
+    "Your methods return value",
+    "Your methods will be tested and proven",
+    "The care and sensitivity you offer in calling functions will return to you"
   ];
 };
